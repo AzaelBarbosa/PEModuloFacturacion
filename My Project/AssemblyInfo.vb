4ca3e1b8ef67a2bc623874b7d8293d6b65fce281
Azael Barbosa <sistemas08@prestamoexpress.com>
2021-06-01 12:15:38 -0500

    Commit para Executables

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' mediante el asterisco ('*'), como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("21.5.27.0")> 
-<Assembly: AssemblyFileVersion("21.5.27.0")> 
+<Assembly: AssemblyVersion("21.6.1.0")> 
+<Assembly: AssemblyFileVersion("21.6.1.0")> 
